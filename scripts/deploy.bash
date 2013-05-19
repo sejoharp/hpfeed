1d27fec5445025401407bd01d9b787391a7a6da0
joscha <zippelman@web.de>
2013-05-19 17:06:51 +0200

    new deploy script for deamontools

--- scripts/deploy.bash
+++ scripts/deploy.bash
@@ -2,7 +2,7 @@
 set -eu
 
 project_dir="/Users/joscha/Documents/workspace/gocode/src/bitbucket.org/joscha/hpfeed"
-destination_ip="10.1.1.3"
+destination="10.1.1.3"
 copy_dir="/home/pi"
 command_dir="/home/hpnews"
 command_user="hpnews"
@@ -15,27 +15,27 @@ cd ${project_dir}
 
 echo "--> compiling binary for ${binary_name}..."
 GOPATH=/Users/joscha/Documents/workspace/gocode/
-env \
-  GOOS=linux \
-  GOARCH=arm \
-  CGO_ENABLED=0 \
-  GOARM=5 \
-  go build
+
+# build command for linux with 64bit
+# GOOS=linux GOARCH=amd64 go build
+
+# build command for linux on arm (raspberry pi)
+GOOS=linux GOARCH=arm CGO_ENABLED=0 GOARM=5 go build
 
 echo "--> uploading via scp ..."
-scp ${binary_name} ${remote_user}@${destination_ip}:${copy_dir}
+scp ${binary_name} ${remote_user}@${destination}:${copy_dir}
 
 echo "--> stopping ${binary_name} ..."
-ssh ${remote_user}@${destination_ip} sudo "/etc/init.d/hpfeed.sh stop"
+ssh ${remote_user}@${destination} sudo "/etc/init.d/hpfeed.sh stop"
 
 echo "--> delete old version ${binary_name} ..."
-ssh ${remote_user}@${destination_ip} sudo "rm ${command_dir}/${binary_name}"
+ssh ${remote_user}@${destination} sudo "rm ${command_dir}/${binary_name}"
 
-echo "--> copying new version to ${command_dir}  ..."
-ssh ${remote_user}@${destination_ip} sudo "mv ${copy_dir}/${binary_name} ${command_dir}/"
+echo "--> moving new version to ${command_dir}  ..."
+ssh ${remote_user}@${destination} sudo "mv ${copy_dir}/${binary_name} ${command_dir}/"
 
 echo "--> setting permissions  ..."
-ssh ${remote_user}@${destination_ip} sudo "chown ${command_user}:${command_group} ${command_dir}/${binary_name}"
+ssh ${remote_user}@${destination} sudo "chown ${command_user}:${command_group} ${command_dir}/${binary_name}"
 
 echo "--> Starting ${binary_name} ..."
-ssh ${remote_user}@${destination_ip} sudo "/etc/init.d/hpfeed.sh start"
+ssh ${remote_user}@${destination} sudo "/etc/init.d/hpfeed.sh start"
---
+++ scripts/deploy_daemontools.bash
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+set -eu
+
+project_dir="/Users/joscha/Documents/workspace/gocode/src/bitbucket.org/joscha/hpfeed"
+destination="10.1.1.3"
+command_dir="/srv/hpeed"
+remote_user="joscha"
+binary_name="hpfeed"
+binary_name_temp="${binary_name}_new"
+
+echo "--> changing to project dir ${project_dir}..."
+cd ${project_dir}
+
+echo "--> compiling binary for ${binary_name}..."
+GOPATH=/Users/joscha/Documents/workspace/gocode/
+
+# build command for linux with 64bit
+GOOS=linux GOARCH=amd64 go build
+
+echo "--> renaming to temporary name ..."
+mv ${binary_name} ${binary_name_temp}
+
+echo "--> uploading via scp ..."
+scp ${binary_name} ${remote_user}@${destination}:${command_dir}
+
+echo "--> stopping ${binary_name} ..."
+ssh ${remote_user}@${destination} "svc -d ~/service/${binary_name}"
+
+echo "--> delete old version ${binary_name} ..."
+ssh ${remote_user}@${destination} "rm ${command_dir}/${binary_name}"
+
+echo "--> renaming new version to ${binary_name}  ..."
+ssh ${remote_user}@${destination} "mv ${command_dir}/${binary_name_temp} ${command_dir}/${binary_name}"
+
+echo "--> Starting ${binary_name} ..."
+ssh ${remote_user}@${destination} "svc -u ~/service/${binary_name}"
