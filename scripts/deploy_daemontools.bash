71e226334b54572ea866675e773fe0ff9a79d5c3
joscha <zippelman@web.de>
2013-05-22 00:09:00 +0200

    bug fix for daemontools deployment

--- scripts/deploy_daemontools.bash
+++ scripts/deploy_daemontools.bash
@@ -4,7 +4,7 @@ set -eu
 project_dir="/Users/joscha/Documents/workspace/gocode/src/bitbucket.org/joscha/hpfeed"
 destination="10.1.1.3"
 remote_user="joscha"
-command_dir="/home/${remote_user}/bin"
+command_dir="/home/${remote_user}/opt/hpfeed/"
 binary_name="hpfeed"
 binary_name_temp="${binary_name}_new"
 
@@ -21,7 +21,7 @@ echo "--> renaming to temporary name ..."
 mv ${binary_name} ${binary_name_temp}
 
 echo "--> uploading via scp ..."
-scp ${binary_name} ${remote_user}@${destination}:${command_dir}
+scp ${binary_name_temp} ${remote_user}@${destination}:${command_dir}
 
 echo "--> stopping ${binary_name} ..."
 ssh ${remote_user}@${destination} "svc -d /home/${remote_user}/service/${binary_name}"
@@ -32,5 +32,11 @@ ssh ${remote_user}@${destination} "rm ${command_dir}/${binary_name}"
 echo "--> renaming new version to ${binary_name}  ..."
 ssh ${remote_user}@${destination} "mv ${command_dir}/${binary_name_temp} ${command_dir}/${binary_name}"
 
+echo "--> changing file permission ${binary_name}  ..."
+ssh ${remote_user}@${destination} "chmod 700 ${command_dir}/${binary_name}"
+
 echo "--> Starting ${binary_name} ..."
 ssh ${remote_user}@${destination} "svc -u /home/${remote_user}/service/${binary_name}"
+
+echo "--> cleaning local temp binary ..."
+rm ${binary_name_temp}
